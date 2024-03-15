8b17909c5fc7b0598392ead2ba3fe71388c2e022
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-15 12:55:34 -0700

    d

--- main.hpp
+++ main.hpp
@@ -13,13 +13,33 @@ int getClosestMed(int, int, int);
 // complete two functions
 int getRandom(void)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+        return rand();
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+int mid=(rdnum3+rdnum1+rdnum2)/3;
+int j=0;
+      for (size_t i = 0; j < 1; i++)
+      {
+        if ((rdnum1-i)==mid||(rdnum1+i)==mid)
+        {
+                return rdnum1;
+                j++;
+        }else if ((rdnum2)-i==mid||(rdnum1+i)==mid)
+        {
+                return rdnum2;
+                j++;
+        }else if ((rdnum3)-i==mid||(rdnum1+i)==mid)
+        {
+                return rdnum3;
+                j++;
+        }
+        
+        
+        
+      }
+      
+      
+      
+                        
 }
